84856395e72afb87214bbf54a3018bdf1c0a093b
gaurav <gaurav18.2013@gmail.com>
2020-03-26 14:58:55 +0530

    First

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//route file
+const bootcamps = require("./routes/bootcamp");
 //load env files
 dotenv.config({ path: "./config/config.env" });
 const app = express();
+
+//mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV}  ${PORT}`)
+  console.log(`server running in  ${process.env.NODE_ENV}  ${PORT}`)
 );
